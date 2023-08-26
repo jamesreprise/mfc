57319231ec45bb2612d62cdefefa50b0364814b1
James Williams <51956016+jamesreprise@users.noreply.github.com>
2023-08-26 14:32:34 +0000

    feat: game server hello world

--- game_server/project.clj
+++ game_server/project.clj
@@ -2,5 +2,9 @@
   :description "MFC Game Server"
   :license {:name "GNU AGPL v3"
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.4.0"]
+                 [ring "1.10.0"]]
+  :ring {:handler game-server.core/game}
+  :plugins [[lein-ring "0.12.6"]]
   :repl-options {:init-ns game-server.core})
--- game_server/src/game_server/core.clj
+++ game_server/src/game_server/core.clj
@@ -1,6 +1,14 @@
-(ns game-server.core)
+(ns game-server.core
+  (:require [clojure.data.json :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn new-players [players]
+  (into {} (map (fn [p] {(key p) (merge {:bux 1500 :position :go} (val p))}) players)))
+
+(defn new-game [players]
+  {:players (new-players players)
+   :turn (nth (rand-nth (into [] players)) 0)})
+
+(defn game [request]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (json/write-str (new-game {"p1" {:piece :car} "p2" {:piece :hat}}))})
