31d1330db9e6c3f91f0a80db394e2085f29f3d4d
James Williams <51956016+jamesreprise@users.noreply.github.com>
2023-09-16 00:06:56 +0000

    feat: string interpolation from core incubator

--- server/project.clj
+++ server/project.clj
@@ -3,6 +3,7 @@
   :license {:name "GNU AGPL v3"
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.incubator "0.1.4"]
                  [org.clojure/tools.logging "1.2.4"]
                  [ch.qos.logback/logback-classic "1.4.11"]
                  [ring "1.10.0"]
@@ -14,6 +15,5 @@
   :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.86.1355"]]}}
   :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   :ring {:handler mfc.server/server}
-  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]
   :plugins [[lein-ring "0.12.6"]]
   :repl-options {:init-ns mfc.server})
