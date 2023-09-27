4ce3900d6788e0e9703ec64151e4848c652c68e6
James Williams <51956016+jamesreprise@users.noreply.github.com>
2023-09-27 21:19:05 +0000

    refactor: board naming cleanup

--- server/src/mfc/game.clj
+++ server/src/mfc/game.clj
@@ -4,22 +4,22 @@
   {-1 :jail/in
    0  :go
    1  :brown/one
-   2  :community-chest/one
+   2  :chest/one
    3  :brown/two
    4  :tax/income
    5  :train/one
-   6  :light-cyan/one
+   6  :cyan/one
    7  :chance/one
-   8  :light-cyan/two
-   9  :light-cyan/three
+   8  :cyan/two
+   9  :cyan/three
    10 :jail/just-visiting
    11 :pink/one
-   12 :infrastructure/electric
+   12 :infra/electric
    13 :pink/two
    14 :pink/three
    15 :train/two
    16 :orange/one
-   17 :community-chest/one
+   17 :chest/one
    18 :orange/two
    19 :orange/three
    20 :free-parking
@@ -30,12 +30,12 @@
    25 :train/three
    26 :yellow/one
    27 :yellow/two
-   28 :infrastructure/water
+   28 :infra/water
    29 :yellow/three
    30 :go-to-jail
    31 :green/one
    32 :green/two
-   33 :community-chest/two
+   33 :chest/two
    34 :green/three
    35 :train/four
    36 :chance/three
